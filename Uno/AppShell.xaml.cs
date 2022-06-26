bd32f27551004adade6ff3203ebc10b5a09c326f
rouni <rouni@DESKTOP-ADPF0DJ>
2022-06-26 14:43:10 -0500

    Add project files.

---
+++ Uno/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Uno;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Uno/ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        string text;
+        public string Text
+        {
+            get => text;
+            set 
+            {
+                text = value;
+                OnPropertyChanged(nameof(text));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name)=>PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
